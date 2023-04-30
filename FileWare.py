1f856d9cf9f2e23c529dd7313de3e2fa29e7a0f6
zhangzebin <qwe788954@163.com>
2023-04-30 15:50:08 +0800

    做了一些修改，增加了注释

--- FileWare.py
+++ FileWare.py
@@ -3,10 +3,11 @@ import http.client
 from util import File_to_Other
 from util import String
 import os
-class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+class Web_Waf(http.server.BaseHTTPRequestHandler):
     file_util = File_to_Other.File_to_Other()
     string_util = String.String()
     cwd_path = os.getcwd()
+
     # 检测是否有恶意的参数传递
     def de_Forward(self,data=""):
         param_list=[]
@@ -26,6 +27,7 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         finally:
             pass
 
+    #检查返回包，判断是否有flag字样，并且进行替换。
     def cg_Response(self,response):
         res = response.read()
         flag=""
@@ -54,8 +56,10 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     # 跳转到jntm.html
     def re_Forward(self,conn_type,port=9870,path="/jntm.html"):
+
         server = "127.0.0.1:{0}".format(port)
         conn = http.client.HTTPConnection(server)
+
         if conn_type == "GET":
             conn.request(conn_type,path)
         elif conn_type == "POST":
@@ -66,30 +70,41 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         return conn.getresponse()
 
 
-    def conn_Access(self):
+    # 如果没有出现敏感参数则放行，但依旧检查返回包是否有flag
+    def conn_Access(self,conn_type):
         # 创建到目标服务器的连接
         conn = http.client.HTTPConnection("localhost:81")
         # 转发 GET 请求
-        conn.request("GET", self.path)
-        print(self.path)
-        # 获取目标服务器的响应
-        response = conn.getresponse()
-        self.cg_Response(response)
+        if conn_type == "GET":
+            conn.request("GET", self.path)
+
+        elif conn_type == "POST":
+            content_length = int(self.headers.get("Content-Length"))
+            body = self.rfile.read(content_length)
+            conn.request("POST", self.path, body=body)
+        # 检测返回包
+        self.cg_Response(conn.getresponse())
+
+    # 拦截GET请求
     def do_GET(self):
         if self.de_Forward():
             print("触发了")
             self.cg_Response(self.re_Forward("GET"))
         else:
             self.conn_Access()
+    # 拦截POST请求
     def do_POST(self):
         if self.de_Forward():
+
             # 创建到目标服务器的连接
             print("触发了")
-            # 转发 POST 请求
-           self.cg_Response(self.re_Forward("POST"))
+            # 转发POST请求
+
+            self.cg_Response(self.re_Forward("POST"))
+
         else:
             self.conn_Access()
 
 
-httpd = http.server.HTTPServer(('localhost', 80), MyHTTPRequestHandler)
+httpd = http.server.HTTPServer(('localhost', 80), Web_Waf)
 httpd.serve_forever()
